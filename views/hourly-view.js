f5a1fd0ac5bff84ef41d741d2af768a2a65ef630
admin-dustbusters <admin@dustbusterscleaners.com>
2025-10-17 09:04:47 -0400

    Create hourly-view.js

---
+++ views/hourly-view.js
@@ -0,0 +1,11 @@
+// Hourly View - Hour-by-Hour Display
+const HourlyView = {
+  render(data) {
+    Utils.toast('Hourly view coming soon!', 'info');
+    // Booking Panel Component
+const BookingPanel = {
+  show(cleaner, date, time) {
+    Utils.toast('Booking panel coming soon!', 'info');
+    // Future implementation for quick booking
+  }
+};
